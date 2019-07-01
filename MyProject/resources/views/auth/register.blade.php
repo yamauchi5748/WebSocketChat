b45be85d947eb7c39ea2dcc8c69d9223962d1413
yamauchi5748 <47198760+yamauchi5748@users.noreply.github.com>
2019-07-01 11:40:49 +0900

    update

--- MyProject/resources/views/auth/register.blade.php
+++ MyProject/resources/views/auth/register.blade.php
@@ -61,6 +61,20 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="api_token" class="col-md-4 col-form-label text-md-right">{{ __('api_token') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="api_token" type="text" class="form-control{{ $errors->has('api_token') ? ' is-invalid' : '' }}" name="api_token" value="{{ old('api_token') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
